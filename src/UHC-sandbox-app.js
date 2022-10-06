70000ab22a63cd914ea30ca90e752af0ff88d017
lorisnikolov55 <loris.nikolov@ucdconnect.ie>
2022-10-06 13:05:10 -0500

    testing patient.html

--- src/UHC-sandbox-app.js
+++ src/UHC-sandbox-app.js
@@ -1,7 +1,7 @@
 async function requestPatientData() {
   const base_url = "https://sandbox.fhir.flex.optum.com/R4"
   console.log(base_url+"Patient")
-  var patient = await fetch(base_url + "Patient", {
+  var patient = await fetch(base_url + "/Patient", {
     headers: {
       Accept: "application/json+fhir",
       Authorization: "Bearer " + myApp.smart.state.tokenResponse.access_token,
