badf27172186c1be694135dc35d73aff361d8395
lorisnikolov55 <loris.nikolov@ucdconnect.ie>
2022-10-10 14:31:44 -0500

    testing auth

--- src/UHC-sandbox-app.js
+++ src/UHC-sandbox-app.js
@@ -15,12 +15,12 @@ async function requestPatientData() {
 }
 
 async function requestAccessToken() {
-  const base_url = "https://sandbox.authz.flex.optum.com/";
-  const auth_url = base_url + "oauth/authorize";
-  const full_auth_url =
-    auth_url +
+  const base_uri = "https://sandbox.authz.flex.optum.com/";
+  const auth_uri = base_uri + "oauth/authorize";
+  const full_auth_uri =
+    auth_uri +
     "?response_type=code&client_id=ad7331f7-7cea-42b5-931a-85b115340836&state=1234zyx&scope=patient%2FPatient.read&redirect_uri=https%3A%2F%2Florisnikolov55.github.io%2FUHC-sandbox-app%2Fpatient.html";
-  console.log(full_auth_url);
+  console.log(full_auth_uri);
 
   let req = new Request(full_auth_uri, {
     method: "GET",
