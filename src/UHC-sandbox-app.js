c8930e9658894962c8b76315e5542a9d524865f3
lorisnikolov55 <loris.nikolov@ucdconnect.ie>
2022-10-10 14:27:10 -0500

    testing

--- src/UHC-sandbox-app.js
+++ src/UHC-sandbox-app.js
@@ -17,10 +17,10 @@ async function requestPatientData() {
 async function requestAccessToken() {
   const base_url = "https://sandbox.authz.flex.optum.com/";
   const auth_url = base_url + "oauth/authorize";
-  const gull_auth_url =
+  const full_auth_url =
     auth_url +
     "?response_type=code&client_id=ad7331f7-7cea-42b5-931a-85b115340836&state=1234zyx&scope=patient%2FPatient.read&redirect_uri=https%3A%2F%2Florisnikolov55.github.io%2FUHC-sandbox-app%2Fpatient.html";
-
-
-  return accessToken;
+  console.log(full_auth_url)
+  
+  //return accessToken
 }
