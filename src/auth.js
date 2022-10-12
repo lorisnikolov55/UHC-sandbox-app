11cec0cd8a978d08e13e279165356c6b7f182ee9
lorisnikolov55 <loris.nikolov@ucdconnect.ie>
2022-10-12 14:43:47 -0500

    restarting auth

--- src/auth.js
+++ src/auth.js
@@ -1,16 +1,1 @@
-import AuthorizationCode from "../node_modules/simple-oauth2";
-//const { AuthorizationCode } = require("simple-oauth2");
-const oauth2 = new AuthorizationCode({
-  client: {
-    id: "ad7331f7-7cea-42b5-931a-85b115340836",
-    secret: "61914843-fc60-4eec-9cfd-13fc2935b083",
-  },
-  auth: {
-    tokenHost: "https://sandbox.authz.flex.optum.com",
-    authorizePath: "/oauth/authorize",
-    tokenPath: "/oauth/token",
-  },
-});
-
-let oauthToken = oauth2.createToken(params);
-console.log(oauthToken);
+// Move Oauth2 flow here once it's finished in launch.htnl
