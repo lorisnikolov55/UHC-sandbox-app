175cb629f7f5ce20f499950ec1a2e3a062cdde73
lorisnikolov55 <loris.nikolov@ucdconnect.ie>
2022-10-12 13:28:42 -0500

    testing module tag

--- src/auth.js
+++ src/auth.js
@@ -1,4 +1,6 @@
-/*const config = {
+import AuthorizationCode from "simple-oauth2";
+//const { AuthorizationCode } = require("simple-oauth2");
+const oauth2 = new AuthorizationCode({
   client: {
     id: "ad7331f7-7cea-42b5-931a-85b115340836",
     secret: "61914843-fc60-4eec-9cfd-13fc2935b083",
@@ -8,34 +10,7 @@
     authorizePath: "/oauth/authorize",
     tokenPath: "/oauth/token",
   },
-};
+});
 
-import { ClientCredentials, ResourceOwnerPassword, AuthorizationCode } from "simple-oauth2";
-
-async function run() {
-  const client = new AuthorizationCode(config);
-
-  const authorizationUri = client.authorizationUri({
-    redirect_uri: "https://lorisnikolov55.github.io/UHC-sandbox-app/patient.html",
-    scope: "patient/Patient.read",
-    state: "1234zyx",
-  });
-
-  // Redirect example using Express (see http://expressjs.com/api.html#res.redirect)
-  res.redirect(authorizationUri);
-  console.log(authorizationUri)
-
-  const tokenParams = {
-    code: "auth_code",
-    redirect_uri: "https://lorisnikolov55.github.io/UHC-sandbox-app/patient.html",
-    scope: "<scope>",
-  };
-
-  try {
-    const accessToken = await client.getToken(tokenParams);
-  } catch (error) {
-    console.log("Access Token Error", error.message);
-  }
-}
-
-run();*/
+let oauthToken = oauth2.createToken(params);
+console.log(oauthToken);
