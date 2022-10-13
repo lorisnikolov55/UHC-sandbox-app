04026a7c6ad6944a5312c40c6e930559b225acba
lorisnikolov55 <loris.nikolov@ucdconnect.ie>
2022-10-12 19:02:12 -0500

    testing

--- src/UHC-sandbox-app.js
+++ src/UHC-sandbox-app.js
@@ -15,11 +15,7 @@ async function requestPatientData() {
 }
 
 async function getAccessCode() {
-  var response = await fetch("https://sandbox.authz.flex.optum.com/oauth/authorize?response_type=code&client_id=ad7331f7-7cea-42b5-931a-85b115340836&state=1234zyx&scope=patient%2FPatient.read&redirect_uri=https%3A%2F%2Florisnikolov55.github.io%2FUHC-sandbox-app%2Fpatient.html", {mode: 'no-cors'}, {
-    headers: {
-      Accept: "application/json+fhir",
-    }
-  })
+  var response = await fetch("https://sandbox.authz.flex.optum.com/oauth/authorize?response_type=code&client_id=ad7331f7-7cea-42b5-931a-85b115340836&state=1234zyx&scope=patient%2FPatient.read&redirect_uri=https%3A%2F%2Florisnikolov55.github.io%2FUHC-sandbox-app%2Fpatient.html", {mode: 'no-cors'})
   .then(function (data) {
     console.log(data);
   });
